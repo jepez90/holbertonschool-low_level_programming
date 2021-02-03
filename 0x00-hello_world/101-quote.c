eb54f1e3138c0263fa1b983db2a053bec4bc4b63
jepez90 <ing.jersonperez@gmail.com>
2021-02-03 23:40:38 +0000

    fix error in file 101, whitespace at linewq removed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,9 +11,9 @@
 */
 int main(void)
 {
-	char texto[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, texto, 59);	
+	write(2, text, 59);
 	return (1);
 }
 
