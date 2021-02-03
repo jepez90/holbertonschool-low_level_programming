22ed714f1e71c100235c6795d2bd3c80a5be4109
jepez90 <ing.jersonperez@gmail.com>
2021-02-03 20:05:49 +0000

    fix error with quotes in file 4, check file 100

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,12 +3,16 @@
 /**
 * main - Entry point
 *
+* print the size of any types
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-	char a;
-
-	printf("Size of a char: %lu byte(s)\n",(unsigned long) sizeof(a));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of a int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long: %lu byte(s)\n", sizeof(long));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
