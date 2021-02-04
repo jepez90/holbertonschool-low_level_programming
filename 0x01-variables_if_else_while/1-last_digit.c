d69624ca1c634d2ebce61db0f617f85b78734d7a
jepez90 <ing.jersonperez@gmail.com>
2021-02-04 20:02:43 +0000

    remove lines that convert n negative in sapositive

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,8 +16,6 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	d = n % 10;
-	if (d < 0)
-		d = -d;
 
 	if (d > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, d);
