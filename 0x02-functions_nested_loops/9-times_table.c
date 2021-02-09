4b6ff3c231f81f73d8a1bba7352d568296591bf7
jepez90 <ing.jersonperez@gmail.com>
2021-02-09 14:41:46 +0000

    change the folder name

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * times_table - prints the time table.
+ *
+ * Return: always 0.
+ */
+void times_table(void)
+{
+	int x, y, xy;
+
+	for (y = 0; y < 10; y++)
+	{
+		for (x = 0; x < 10; x++)
+		{
+			xy = x * y;
+			if (x != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (xy > 9)
+			{
+				_putchar(xy / 10 + '0');
+				_putchar(xy % 10 + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(xy % 10 + '0');
+			}
+		}
+		_putchar(10);
+	}
+
+}
