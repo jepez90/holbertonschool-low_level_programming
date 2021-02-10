8a426e0b7527cce4c65825bd6db5d8e6dcf025c9
jepez90 <ing.jersonperez@gmail.com>
2021-02-10 01:00:32 +0000

    redy to check

---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,17 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+/* positive_or_negative - print */
+void positive_or_negative(int i);
+
+/* largest_number - return the largest number. */
+int largest_number(int, int, int);
+
+/* print_remaining_days - print days to end of year. */
+void print_remaining_days(int, int, int);
+
+/* covert_day - return days*/
+int convert_day(int, int);
+#endif /* HOLBERTON_H */
