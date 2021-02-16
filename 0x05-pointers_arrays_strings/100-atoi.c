d7ac0e8768a711169b26be8c25936139ce8e69b8
jepez90 <ing.jersonperez@gmail.com>
2021-02-16 16:33:13 +0000

    modify the function to avoid overflow

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,25 +10,34 @@ int _atoi(char *s)
 {
 	int sign = 1;
 	int number = 0;
-
 	/*1- analisys sign*/
 	while (!('0' <= *s && *s <= '9') && *s != '\0')
 	{
 		if (*s == '-')
 			sign *= -1;
-
 		if (*s == '+')
 			sign *= +1;
-
 		s++;
 	}
 
 	/*2 - extract the number */
 	while ('0' <= *s && *s <= '9' && *s != '\0')
 	{
+		/*3 avoid overflow */
+		if (sign > 0)
+		{
+			if ((number * 10) + (*s - '0') > INT_MAX)
+				break;
+		}
+		else
+		{
+			if (-((number * 10) + (*s - '0')) < INT_MIN)
+				break;
+		}
+
 		number = (number * 10) + (*s - '0');
 		s++;
 	}
-
 	return (number * sign);
 }
+
