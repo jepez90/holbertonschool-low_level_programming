d469d5ef7da4921e4048b6ea01f9c56493fdd3d9
jepez90 <ing.jersonperez@gmail.com>
2021-02-17 02:31:16 +0000

    file 101 added

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+  * main - check the code for Holberton School students.
+  *
+  * Return: Always 0.
+  */
+int main(void)
+{
+	int i;
+
+	srand(time(NULL));
+	for (i=0 ;i<=37;i++)
+		putchar(rand() % (122-33) + 33);
+
+
+	return (0);
+}
