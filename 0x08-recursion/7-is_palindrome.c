fc4709164745ea872b135d66ca892293d3a04619
jepez90 <ing.jersonperez@gmail.com>
2021-02-25 02:56:11 +0000

    file 7 modified, 2 functions added

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,6 +1,40 @@
 #include "holberton.h"
 
 /**
+ * _is_palindrome - returns 1 if a string is a palindrome and 0 if not.
+ *
+ * @s: Pointer to string
+ * @i: poaicion relativa del caracter a compaar
+ *
+ * Return: 1 if the tring is palindrome or 0 if it isn't.
+ */
+int _is_palindrome(char *s, int i)
+{
+	if (i <= 1)
+		return (1);
+
+	if (*s == *(s + i))
+		return (_is_palindrome(s + 1, i - 2));
+	else
+		return (0);
+}
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ *
+ * @s: Pointer to string
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	else
+		return (0);
+}
+
+/**
  * is_palindrome - returns 1 if a string is a palindrome and 0 if not.
  *
  * @s: Pointer to string
@@ -9,25 +43,8 @@
  */
 int is_palindrome(char *s)
 {
-	int i;
-	char subs[30];
+	int i = _strlen_recursion(s);
 
-	for (i = 0; *(s + i) != '\0'; i++)
-	{
-		if (i > 0)
-			subs[i - 1] = s[i];
-	}
-	if (i >= 2)
-		subs[i - 2] = '\0';
-
-	if (i <= 1)
-		return (1);
-
-	if (*s == *(s + i - 1))
-	{
-		return (is_palindrome(subs));
-	}
-	else
-		return (0);
-
+	return (_is_palindrome(s, i - 1));
 }
+
