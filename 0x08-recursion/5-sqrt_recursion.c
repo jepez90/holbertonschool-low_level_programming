96b2c45a87b9606f04faef0f4e48805ac77a89a3
jepez90 <ing.jersonperez@gmail.com>
2021-02-25 00:32:43 +0000

    project 8 added

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ *
+ * @n: number to calculate its sqrt
+ *
+ * Return: sqrt of n
+ */
+int _sqrt_recursion(int n)
+{
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+else
+		return (x * _pow_recursion(x, y - 1));
+}
