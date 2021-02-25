d5d172f9a9456bc2dbdcb6a0038c1c57a2468bc0
jepez90 <ing.jersonperez@gmail.com>
2021-02-25 03:22:30 +0000

    file 6 modified, add another function, delete variable static

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,28 @@
 #include "holberton.h"
 
 /**
+ * _is_prime_number -  returns 1 or 0 if the input integer is a prime # or not
+ *
+ * @n: number to check if ti is prime number
+ * @cont: number to check if divide to n exactly
+ *
+ * Return: 1 if n is prime number, 0 if it is not.
+ */
+int _is_prime_number(int n, int cont)
+{
+	if (n % cont == 0 || n < 2)
+	{
+		return (0);
+	}
+	else if (cont > n / 2)
+	{
+		return (1);
+	}
+	return (_is_prime_number(n, cont + 1));
+}
+
+
+/**
  * is_prime_number -  returns 1 or 0 if the input integer is a prime # or not
  *
  * @n: number to check if ti is prime number
@@ -9,17 +31,5 @@
  */
 int is_prime_number(int n)
 {
-	static int cont = 2;
-
-		if (n % cont == 0 || n < 2)
-		{
-			return 0;
-		}
-		else if (cont > n/2)
-		{
-			return 1;
-		}
-
-	cont++;
-	return is_prime_number (n);
+	return (_is_prime_number(n, 2));
 }
