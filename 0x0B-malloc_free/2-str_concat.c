c5714cd5fe1c96ea456a08ed576904b2003b6b6d
Jerson Perez <ing.jersonperez@gmail.com>
2021-03-03 00:53:19 +0000

    fix error in line 25 where work with s2 instead s1

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
  * str_concat - concatenates two strings.
  *
@@ -14,15 +14,15 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2;
 
 	if (s1 == NULL)
-		len1 = 0;
+		len1 = 1;
 	else
 		for (len1 = 0; *(s1 + len1) != '\0'; len1++)
 		{}
 
 	if (s2 == NULL)
-		len2 = 0;
+		len2 = 1;
 	else
-		for (len2 = 0; *(s1 + len2) != '\0'; len2++)
+		for (len2 = 0; *(s2 + len2) != '\0'; len2++)
 		{}
 
 	resultado = malloc(sizeof(char) * (len1 + len2 - 1));
