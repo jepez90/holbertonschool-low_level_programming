374fede95cabef4bc97ce7edbbd19a94f206ba25
Jerson Perez <ing.jersonperez@gmail.com>
2021-03-03 01:00:09 +0000

    change the condition when the strings are void

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,13 +13,13 @@ char *str_concat(char *s1, char *s2)
 	char *resultado;
 	int len1, len2;
 
-	if (s1 == NULL)
+	if (*s1 == '\0')
 		len1 = 1;
 	else
 		for (len1 = 0; *(s1 + len1) != '\0'; len1++)
 		{}
 
-	if (s2 == NULL)
+	if (*s2 == '\0')
 		len2 = 1;
 	else
 		for (len2 = 0; *(s2 + len2) != '\0'; len2++)
