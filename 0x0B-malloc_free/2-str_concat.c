c8667ca253bb8160c0bf4c05eae789fb2488d7a7
Jerson Perez <ing.jersonperez@gmail.com>
2021-03-03 03:52:46 +0000

    uptdate code, remove condition for void strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,36 +14,29 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2;
 
 	if (s1 == NULL)
-		len1 = 1;
-	else if (*s1 == '\0')
-		len1 = 1;
-	else
-		for (len1 = 0; *(s1 + len1) != '\0'; len1++)
-		{}
+		s1 = "";
+	for (len1 = 0; s1[len1]; len1++)
+	{}
 
 	if (s2 == NULL)
-		len2 = 1;
-	else if (*s2 == '\0')
-		len2 = 1;
-	else
-		for (len2 = 0; *(s2 + len2) != '\0'; len2++)
-		{}
+		s2 = "";
+	for (len2 = 0; s2[len2]; len2++)
+	{}
 
 	resultado = malloc(sizeof(char) * (len1 + len2 - 1));
 	if (resultado == NULL)
 		return (NULL);
 
-	len1 = 0;
-	if (s1 != NULL)
-		for (; *(s1 + len1) != '\0'; len1++)
-			*(resultado + len1) = *(s1 + len1);
 
-	if (s2 != NULL)
-		for (len2 = 0; *(s2 + len2) != '\0'; len2++)
-		{
-			*(resultado + len1) = *(s2 + len2);
-			len1++;
-		}
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+		resultado[len1] = s1[len1];
+
+
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+	{
+		resultado[len1] = s2[len2];
+		len1++;
+	}
 
 	*(resultado + len1) = '\0';
 	return (resultado);
