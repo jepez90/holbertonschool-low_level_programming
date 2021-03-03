989c49fcc0fbbfa829812ab660ad7b3d4c7b4d1f
Jerson Perez <ing.jersonperez@gmail.com>
2021-03-03 01:12:30 +0000

    add conditions when the pointers are null

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,13 +13,17 @@ char *str_concat(char *s1, char *s2)
 	char *resultado;
 	int len1, len2;
 
-	if (*s1 == '\0')
+	if (s1 == NULL)
+		len1 = 1;
+	else if ( *s1 == '\0')
 		len1 = 1;
 	else
 		for (len1 = 0; *(s1 + len1) != '\0'; len1++)
 		{}
 
-	if (*s2 == '\0')
+	if (s2 == NULL)
+		len2 = 1;
+	else if ( *s2 == '\0')
 		len2 = 1;
 	else
 		for (len2 = 0; *(s2 + len2) != '\0'; len2++)
@@ -29,16 +33,18 @@ char *str_concat(char *s1, char *s2)
 	if (resultado == NULL)
 		return (NULL);
 
-	for (len1 = 0; *(s1 + len1) != '\0'; len1++)
-	{
-		*(resultado + len1) = *(s1 + len1);
-	}
+	len1 = 0;
+	if (s1 != NULL)
+		for (; *(s1 + len1) != '\0'; len1++)
+			*(resultado + len1) = *(s1 + len1);
 
-	for (len2 = 0; *(s2 + len2) != '\0'; len2++)
-	{
-		*(resultado + len1) = *(s2 + len2);
-		len1++;
-	}
+	if (s2 != NULL)
+		for (len2 = 0; *(s2 + len2) != '\0'; len2++)
+		{
+			*(resultado + len1) = *(s2 + len2);
+			len1++;
+		}
+
 	*(resultado + len1) = '\0';
 	return (resultado);
 }
