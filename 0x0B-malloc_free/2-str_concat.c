a84466c8f4a7d1f44c5f1dcc612ce2e503a2ce08
Jerson Perez <ing.jersonperez@gmail.com>
2021-03-03 04:13:14 +0000

    update code for len calculate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,19 +11,20 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *resultado;
-	int len1, len2;
+	int len1 = 0, len2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
-	for (len1 = 0; s1[len1]; len1++)
-	{}
+	while (s1[len1])
+		len1++;
+
 
 	if (s2 == NULL)
 		s2 = "";
-	for (len2 = 0; s2[len2]; len2++)
-	{}
+	while (s2[len2])
+		len2++;
 
-	resultado = malloc(sizeof(char) * (len1 + len2 - 1));
+	resultado = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (resultado == NULL)
 		return (NULL);
 
