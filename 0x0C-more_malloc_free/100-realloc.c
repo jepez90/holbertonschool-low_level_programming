f8aacd9d38b9a2c7beb1c9438ad36ea56ad0a9ca
Jerson Perez <ing.jersonperez@gmail.com>
2021-03-05 04:39:02 +0000

    file 100 ready to check

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _realloc - reallocates a memory block using malloc and free.
+ *
+ * @ptr: pointer to be reallocated
+ * @old_size: initial size of ptr
+ * @new_size: final size of ptr
+ *
+ * Return: pointer to the string result
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *result;
+	unsigned int i = 0;
+
+	if (old_size == new_size)
+		return (ptr);
+
+	if (new_size == 0)
+	{
+		if (ptr != NULL)
+			free(ptr);
+
+		return (NULL);
+	}
+	else
+	{
+		result = malloc(new_size * sizeof(result));
+		if (result != NULL && ptr != NULL)
+		{
+			for (i = 0; i < old_size && i < new_size; i++)
+				*(result + i) = *((char *)ptr + i);
+
+			/*(result + i) = '\0';*/
+		}
+
+		if (ptr != NULL)
+			free(ptr);
+
+		return (result);
+	}
+}
