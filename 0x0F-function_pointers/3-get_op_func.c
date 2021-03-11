3d44c4c662f6494679cc6d051aa4776ada9670b8
Jerson Perez <ing.jersonperez@gmail.com>
2021-03-11 00:08:12 +0000

    check if operator is of len 1

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -24,7 +24,7 @@ int (*get_op_func(char *s))(int num1, int num2)
 
 	while (ops[i].op != NULL)
 	{
-		if (*(ops[i].op) == *s)
+		if (*(ops[i].op) == *s && *(s + 1) == '\0')
 			return (ops[i].f);
 		i++;
 	}
