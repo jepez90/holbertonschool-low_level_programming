c93380db808d74e86de292095b11dbaafe976030
Jerson Perez <ing.jersonperez@gmail.com>
2021-03-11 14:33:10 +0000

    change conditional in line 27

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -24,7 +24,7 @@ int (*get_op_func(char *s))(int num1, int num2)
 
 	while (ops[i].op)
 	{
-		if (*(ops[i].op) == *s && *(s + 1) == '\0')
+		if ((ops[i])->op == *s && *(s + 1) == '\0')
 			return (ops[i].f);
 
 		i++;
