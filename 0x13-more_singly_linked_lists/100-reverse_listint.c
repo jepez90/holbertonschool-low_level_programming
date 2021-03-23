2ab00b8841d6e4e57a3ffd8e940e2dfb05e81ade
Jerson Perez <ing.jersonperez@gmail.com>
2021-03-23 22:05:15 +0000

    fix error with betty style :(

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -10,13 +10,14 @@
 listint_t *reverse_listint(listint_t **head)
 {
 	*head = _reverse_listint(head, NULL);
-	return(*head);
+	return (*head);
 }
 
 
 listint_t *_reverse_listint(listint_t **head, listint_t *prev_address)
 {
 	listint_t *new_head = NULL;
+
 	if (*head)
 		new_head = _reverse_listint(&(*head)->next, *head);
 	else
