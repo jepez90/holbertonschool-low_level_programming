302c0c6ef98e7e8d1726802742431b4172cce285
Jerson Perez <ing.jersonperez@gmail.com>
2021-03-30 00:03:45 +0000

    Fix error on handler text_content == NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,10 +26,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (!text_content)
 	{
-		*text_content = '\n';
+		text_content = "";
 	}
 
-	new_file = open(filename, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+	new_file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (new_file == -1)
 	{
 		return (-1);
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -26,7 +26,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!text_content)
 	{
-		*text_content = '\n';
+		text_content = "";
 	}
 
 	opened_file = open(filename, O_WRONLY | O_APPEND);
