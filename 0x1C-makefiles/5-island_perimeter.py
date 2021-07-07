ff0f27846774b3e8b53491d35bd8737f4eeb9827
jepez90 <ing.jersonperez@gmail.com>
2021-07-06 20:12:51 -0500

    [Add] all files for task 0xC

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+ #!/usr/bin/python3
+"""this module defines an function to calculate a perimeter in a grid"""
+
+def island_perimeter(grid):
+    """This function returns the perimeter of the island described in grid
+
+    args:
+        Grid([[int]]): is a list of list of integers where:
+        0 represents a water zone
+        1 represents a land zone
+        One cell is a square with side length 1
+        Grid cells are connected horizontally/vertically (not diagonally).
+        Grid is rectangular, width and height don’t exceed 100
+    """
+    perimeter = 0
+    for row_index, row in enumerate(grid):
+        for cell_index, cell in enumerate(row):
+            if cell == 1:
+                perimeter += (1 - grid[row_index - 1][cell_index])# north
+                perimeter += (1 - grid[row_index][cell_index + 1])# east
+                perimeter += (1 - grid[row_index + 1][cell_index])# south
+                perimeter += (1 - grid[row_index][cell_index - 1])# west
+    return perimeter
---
+++ 0x1C-makefiles/tests/5-main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+5-main
+"""
+import sys
+import os
+sys.path.append(os.path.abspath('..'))
+island_perimeter = __import__('5-island_perimeter').island_perimeter
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 1, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
