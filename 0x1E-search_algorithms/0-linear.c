f6f071a3d38b51402c34e3848f0db4e1cb4a4a94
jepez90 <ing.jersonperez@gmail.com>
2021-08-30 20:46:12 -0500

    [Fix] document style

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -3,9 +3,11 @@
 /**
  * linear_search - searches for a value in an array of integers using the
  * Linear search algorithm
+ *
  * @array: is a pointer to the first element of the array to search in.
  * @size: is the number of elements in array.
  * @value: is the value to search for.
+ *
  * Return: the first index where value is located or -1 if value is not present
  * in array or if array is NULL
  */
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -5,28 +5,30 @@ void print_array(int *array, size_t size);
 /**
  * binary_search - searches for a value in a sorted array of integers using
  * the Binary search algorithm
+ *
  * @array: is a pointer to the first element of the array to search in.
  * @size: is the number of elements in array.
  * @value: is the value to search for.
+ *
  * Return: the first index where value is located or -1 if value is not present
  * in array or if array is NULL
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t index, range_begin = 0, range_end = size - 1;
+	size_t index, range_start = 0, range_end = size - 1;
 
 	if (array != NULL)
 	{
-		while (range_begin <= range_end)
+		while (range_start <= range_end)
 		{
-			print_array(array + range_begin, range_end - range_begin + 1);
+			print_array(array + range_start, range_end - range_start + 1);
 
-			index = (range_end + range_begin) / 2;
+			index = (range_end + range_start) / 2;
 
 			if (array[index] == value)
 				return (index);
 			else if (array[index] < value)
-				range_begin = index + 1;
+				range_start = index + 1;
 			else
 				range_end = index - 1;
 		}
@@ -36,8 +38,10 @@ int binary_search(int *array, size_t size, int value)
 
 /**
  * print_array - prints an array of integers
+ *
  * @array: is a pointer to the first element of the array to print.
  * @size: is the number of elements in array.
+ *
  * Return: nothing
  */
 void print_array(int *array, size_t size)
