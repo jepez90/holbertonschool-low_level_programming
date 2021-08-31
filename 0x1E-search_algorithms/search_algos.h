c2a36503be8ac9a9bf3c6009e0c93f45f5e823b7
jepez90 <ing.jersonperez@gmail.com>
2021-08-31 13:07:33 -0500

    [Add]  guarded for the header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,10 @@
+#ifndef SEARC_ALGOS
+#define SEARC_ALGOS
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+
+
 
 /**
  * struct listint_s - singly linked list
@@ -27,3 +31,4 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+#endif
