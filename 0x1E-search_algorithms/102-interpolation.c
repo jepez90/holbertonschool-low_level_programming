a2be1e919ab0bd5a1babbc7f3799468b3928c8c7
jepez90 <ing.jersonperez@gmail.com>
2021-09-02 09:45:31 -0500

    [Fix] message of out of range

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -30,7 +30,7 @@ int interpolation_search(int *array, size_t size, int value)
 
 			if (index >= size)
 			{
-				printf("Value checked array[%ld] is out of range)\n", index);
+				printf("Value checked array[%ld] is out of range\n", index);
 				break;
 			}
 
